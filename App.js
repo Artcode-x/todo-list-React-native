dafbd94933db873612365004c549e315aad8f31b
Alex <aironer@yandex.ru>
2024-07-22 23:43:12 +0300

    Создание инпута и кнопки для добавления задач 1. Создание обрабочтика для добавления задач 2. При вводе текста в инпут сохраняем все в стейт

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { FlatList, StyleSheet, Text, View } from "react-native"
 import Header from "./components/Header"
 import { useState } from "react"
 import ListItem from "./components/ListItem"
+import Form from "./components/Form"
 
 export default function App() {
   const [listOfItems, setListOfItems] = useState([
@@ -11,9 +12,23 @@ export default function App() {
     { text: "Проверить биржу", index: 3 },
     { text: "Проверить кошелек", index: 4 },
   ])
+
+  const addHandler = (text) => {
+    // list - это весь список, что выше
+    setListOfItems((list) => {
+      // Будем возвращать новый список,
+      return [
+        { text: text, index: 5 },
+        // передаем также тот список который есть на данный момент с пом-ю оператора spread:
+        ...list,
+      ]
+    })
+  }
+
   return (
     <View>
       <Header />
+      <Form addHandler={addHandler} />
       <View>
         <FlatList data={listOfItems} renderItem={({ item }) => <ListItem el={item} />} />
       </View>
