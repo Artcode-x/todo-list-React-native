cfeaf5f790f99bf5f0180fb9b0c9cffc104ea789
Alex <aironer@yandex.ru>
2024-07-22 23:01:39 +0300

    отображаем список

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { FlatList, StyleSheet, Text, View } from "react-native"
 import Header from "./components/Header"
+import { useState } from "react"
 
 export default function App() {
+  const [listOfItems, setListOfItems] = useState([
+    { text: "Купить молоко", index: 1 },
+    { text: "Помыть машину", index: 2 },
+    { text: "Проверить биржу", index: 3 },
+    { text: "Проверить кошелек", index: 4 },
+  ])
   return (
     <View>
       <Header />
-      <View></View>
+      <View>
+        <FlatList data={listOfItems} renderItem={({ item }) => <Text>{item.text}</Text>} />
+      </View>
     </View>
   )
 }
